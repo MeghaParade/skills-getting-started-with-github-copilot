9dfc8508d52697714cee1d60dc7b7883b838e180
Megha Shivaji Parade <megha.parade@gmail.com>
2026-01-19 07:42:15 +0000

    added

--- src/app.py
+++ src/app.py
@@ -55,20 +55,25 @@ def get_activities():
 @app.post("/activities/{activity_name}/signup")
 def signup_for_activity(activity_name: str, email: str):
     """Sign up a student for an activity"""
+
     # Validate activity exists
     if activity_name not in activities:
         raise HTTPException(status_code=404, detail="Activity not found")
 
     # Get the specific activity
     activity = activities[activity_name]
+
+    # Validate student is not already signed up 
+
+
     # Validate student is not already signed up
-    if email in activity["participants"]: 
+    if email in activity["participants"]:
         raise HTTPException(status_code=400, detail="Student is already signed up")
-    
+
     # Check if activity is at max capacity
     if len(activity["participants"]) >= activity["max_participants"]:
         raise HTTPException(status_code=400, detail="Activity is at max capacity")
-    
+
     # Add student
     activity["participants"].append(email)
     return {"message": f"Signed up {email} for {activity_name}"}
